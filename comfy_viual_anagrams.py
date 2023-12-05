f106119004a42d74a447dfdadab18cd71b9dcb9b
zcfrank1st <zcfrank1st@foxmail.com>
2023-12-05 13:05:23 +0800

    fix

--- comfy_viual_anagrams.py
+++ comfy_viual_anagrams.py
@@ -84,12 +84,12 @@ class VisualAnagramsSampleNode:
                                 reduction='mean',
                                 noise_level=50,
                                 generator=generator)
-        image_tensor = torch.squeeze(image)
-        image_np = image_tensor.cpu().numpy()  # Change from CxHxW to HxWxC for Pillow
+        
+        image_np = image.cpu().numpy()  # Change from CxHxW to HxWxC for Pillow
         im = Image.fromarray((image_np.squeeze(0) * 255).astype(np.uint8))
         image_tensor_out = torch.tensor(np.array(im).astype(np.float32) / 255.0)  # Convert back to CxHxW
         image_tensor_out = torch.unsqueeze(image_tensor_out, 0)
-        return (image_tensor_out / 2. + 0.5, )
+        return (image_tensor_out, )
 
 class VisualAnagramsAnimateNode:
     ...
